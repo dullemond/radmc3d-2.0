afc05da5b1356288ff4ea0b03b7f03ed34e70390
Cornelis Dullemond <dullemond@uni-heidelberg.de>
2020-08-24 12:27:49 +0200

    Adding tools/simpleread.py which is a simple version of radmc3dPy with only the most basic reading stuff. Useful for beginners. Still in progress.

---
+++ python/tools/simpleread.py
@@ -0,0 +1,329 @@
+"""
+SIMPLEREAD
+
+by C.P. Dullemond and A. Juhasz (2020)
+
+This Python script contains a few very basic functions for reading the input
+and output files of RADMC-3D. They are mostly meant for pedagogical purpose: to
+get a feeling for the structure of these files, and for those who just started
+with RADMC-3D. These functions only read the ascii versions of these files, not
+the binary versions. Also, they are very incomplete, and work only for some very
+simple models (e.g. no octree grids, only simple dust opacities, no line data
+files).
+
+Usage: In a model directory, in Python (use Python 3):
+
+  from radmc3d_tools.simpleread import *
+
+You can then read, for instance, the dust temperature computed by RADMC-3D
+using the radmc3d mctherm command:
+
+  d = read_dusttemp()
+
+Then d contains d.grid and d.dusttemp, the latter being the array of dust
+temperatures. There are various read_***() functions: have a look in this
+script file.
+
+For real scientific use, it is recommended to use the radmc3dPy package, which is
+also included in the RADMC-3D distribution. It is located here (starting from the
+root of the RADMC-3D package):
+
+  python/radmc3dPy/
+
+"""
+from __future__ import print_function
+
+try:
+    import numpy as np
+except ImportError:
+    np = None
+    print(' Numpy cannot be imported ')
+    print(' To use the python module of RADMC-3D you need to install Numpy')
+    print(traceback.format_exc())
+
+try:
+    import matplotlib.pyplot as plt
+except ImportError:
+    plt = None
+    print('Warning')
+    print('matplotlib.pyplot cannot be imported')
+    print('Without matplotlib you can use the python module to set up a model but you will not be able to plot things')
+    print('or display images')
+
+class simplereaddata(object):
+    """
+    Generic data object for the RADMC-3D simpleread.py functions.
+    """
+    def __init__(self,datatype):
+        self.datatype = datatype
+
+def read_grid():
+    """
+    Reading the amr_grid.inp file, but only for regular grids (not for octree ones). 
+
+    ARGUMENTS:
+      None
+
+    RETURNS:
+      Data object containing:
+
+        .nx          Nr of cells in x direction (in spherical coordinates this is the r-direction)
+        .ny          Nr of cells in y direction (in spherical coordinates this is the theta-direction)
+        .nz          Nr of cells in z direction (in spherical coordinates this is the phi-direction)
+        .nxi etc     The same as .nx etc, but now +1, giving the number of cell interfaces
+        .crd_sys     String indicating the coordinate system ('car' or 'sph')
+        .x,.y,.z     The x, y and z grid cell center locations
+        .xi,.yi,.zi  The x, y and z grid cell interface locations (each array 1 longer than the x, y, z ones)
+
+    """
+    grid  = simplereaddata('grid')
+    fname = 'amr_grid.inp'
+    print('Reading '+fname)
+    data  = np.fromfile(fname, count=-1, sep=" ", dtype=np.float64)
+
+    # Read the header
+    hdr   = np.array(data[:10], dtype=np.int)
+    data  = data[10:]
+
+    # Check the file format
+    if hdr[0] != 1:
+        msg = 'Unknown format number in amr_grid.inp'
+        raise RuntimeError(msg)
+    if hdr[2] < 100:
+        grid.crd_sys = 'car'
+    elif (hdr[2] >= 100) & (hdr[2] < 200):
+        grid.crd_sys = 'sph'
+    else:
+        raise ValueError('Unsupported coordinate system identification in the ' + fname + ' file.')
+
+    # Get active dimensions
+    grid.act_dim = hdr[4:7]
+
+    # Get the number of cells in each dimensions
+    grid.nx = hdr[7]
+    grid.ny = hdr[8]
+    grid.nz = hdr[9]
+    grid.nxi, grid.nyi, grid.nzi = grid.nx + 1, grid.ny + 1, grid.nz + 1
+
+    # Get the cell interfaces
+    grid.xi = data[:grid.nxi]
+    data = data[grid.nxi:]
+    grid.yi = data[:grid.nyi]
+    data = data[grid.nyi:]
+    grid.zi = data[:grid.nzi]
+
+    # Compute the cell centers
+    if grid.crd_sys == 'car':
+        grid.x = (grid.xi[0:grid.nx] + grid.xi[1:grid.nx + 1]) * 0.5
+        grid.y = (grid.yi[0:grid.ny] + grid.yi[1:grid.ny + 1]) * 0.5
+        grid.z = (grid.zi[0:grid.nz] + grid.zi[1:grid.nz + 1]) * 0.5
+    else:
+        grid.x = np.sqrt(grid.xi[0:grid.nx] * grid.xi[1:grid.nx + 1])
+        grid.y = (grid.yi[0:grid.ny] + grid.yi[1:grid.ny + 1]) * 0.5
+        grid.z = (grid.zi[0:grid.nz] + grid.zi[1:grid.nz + 1]) * 0.5
+
+    # Now return the grid object
+    return grid
+    
+def read_dustdens(indexorder='fortran'):
+    """
+    Reading the dust_density.inp file, but only for regular grids, and only
+    for text data format (not binary).
+
+    ARGUMENTS:
+      indexorder        If 'fortran' then converting array to fortran 
+                        index order (default). Else use Python/C order.
+
+    RETURNS:
+      Data object containing:
+
+        .grid           A grid object (see read_grid())
+        .rhodust        An array with the dust density values (in g/cm^3)
+    """
+    grid      = read_grid()
+    dustdens  = simplereaddata('dust_density')
+    dustdens.grid = grid
+    fname     = 'dust_density.inp'
+    print('Reading '+fname)
+    data      = np.fromfile(fname, count=-1, sep=" ", dtype=np.float64)
+    
+    # Read the header
+    hdr       = np.array(data[:3], dtype=np.int)
+    data      = data[3:]
+
+    # Check the file format
+    if hdr[0] != 1:
+        msg = 'Unknown format number in dust_density.inp'
+        raise RuntimeError(msg)
+
+    # Get the number of cells, and check against
+    nrcells = grid.nx*grid.ny*grid.nz
+    if(hdr[1]!=nrcells):
+        msg = 'Number of grid cells in dust_density.inp inconsistent with amr_grid.inp'
+        raise RuntimeError(msg)
+
+    # Get the number of dust species
+    dustdens.nrspec = hdr[2]
+
+    # Convert the rest of the data to the proper shape
+    data = np.reshape(data, [dustdens.nrspec, grid.nz, grid.ny, grid.nx])
+
+    # If indexorder is set to 'fortran', then the inner index of the array
+    # should be left (even though in Python the inner index is right). This
+    # is to assure that the index order in the Python arrays is the same as
+    # in the RADMC-3D code. But by setting indexorder to anything else, you
+    # can keep Python natural order (which is equal to C index order), in
+    # which the inner index is the rightmost index.
+    if indexorder=='fortran':
+        data = np.swapaxes(data, 0, 3)
+        data = np.swapaxes(data, 1, 2)
+
+    # Now add this to the object
+    dustdens.rhodust = data
+
+    # Return the dustdens object
+    return dustdens
+
+def read_dusttemp(indexorder='fortran'):
+    """
+    Reading the dust_temperature.dat file, but only for regular grids, and only
+    for text data format (not binary).
+
+    ARGUMENTS:
+      indexorder        If 'fortran' then converting array to fortran 
+                        index order (default). Else use Python/C order.
+
+    RETURNS:
+      Data object containing:
+
+        .grid           A grid object (see read_grid())
+        .dusttemp       An array with the dust tempeature values (in K)
+    """
+    grid      = read_grid()
+    dusttemp  = simplereaddata('dust_temperature')
+    dusttemp.grid = grid
+    fname     = 'dust_temperature.dat'
+    print('Reading '+fname)
+    data      = np.fromfile(fname, count=-1, sep=" ", dtype=np.float64)
+    
+    # Read the header
+    hdr       = np.array(data[:3], dtype=np.int)
+    data      = data[3:]
+
+    # Check the file format
+    if hdr[0] != 1:
+        msg = 'Unknown format number in dust_temperature.inp'
+        raise RuntimeError(msg)
+
+    # Get the number of cells, and check against
+    nrcells = grid.nx*grid.ny*grid.nz
+    if(hdr[1]!=nrcells):
+        msg = 'Number of grid cells in dust_temperature.inp inconsistent with amr_grid.inp'
+        raise RuntimeError(msg)
+
+    # Get the number of dust species
+    dusttemp.nrspec = hdr[2]
+
+    # Convert the rest of the data to the proper shape
+    data = np.reshape(data, [dusttemp.nrspec, grid.nz, grid.ny, grid.nx])
+
+    # If indexorder is set to 'fortran', then the inner index of the array
+    # should be left (even though in Python the inner index is right). This
+    # is to assure that the index order in the Python arrays is the same as
+    # in the RADMC-3D code. But by setting indexorder to anything else, you
+    # can keep Python natural order (which is equal to C index order), in
+    # which the inner index is the rightmost index.
+    if indexorder=='fortran':
+        data = np.swapaxes(data, 0, 3)
+        data = np.swapaxes(data, 1, 2)
+
+    # Now add this to the object
+    dusttemp.dusttemp = data
+
+    # Return the dusttemp object
+    return dusttemp
+
+def read_image(indexorder='fortran'):
+    """
+    Reading the image.out file.
+
+    ARGUMENTS:
+      indexorder        If 'fortran' then converting array to fortran 
+                        index order (default). Else use Python/C order.
+
+    RETURNS:
+      Data object containing:
+
+        .freq           Frequency at which the image is taken
+        .image          An array with the image intensity in erg/(s.cm^2.Hz.ster)
+    """
+    pc        = 3.08572e18     # Parsec                  [cm]
+    cc        = 2.99792458e10  # Light speed             [cm/s]
+    image     = simplereaddata('image')
+    fname     = 'image.out'
+    print('Reading '+ fname)
+    with open(fname, 'r') as rfile:
+        dum = ''
+
+        # Format number
+        iformat = int(rfile.readline())
+
+        # Nr of pixels
+        dum = rfile.readline()
+        dum = dum.split()
+        image.nx = int(dum[0])
+        image.ny = int(dum[1])
+
+        # Nr of frequencies
+        image.nfreq = int(rfile.readline())
+        image.nwav = image.nfreq
+
+        # Pixel sizes
+        dum = rfile.readline()
+        dum = dum.split()
+        image.sizepix_x = float(dum[0])
+        image.sizepix_y = float(dum[1])
+
+        # Wavelength of the image
+        image.wav = np.zeros(image.nwav, dtype=np.float64)
+        for iwav in range(image.nwav):
+            image.wav[iwav] = float(rfile.readline())
+        image.freq = cc / image.wav * 1e4
+
+        # Read the rest of the data
+        data = np.fromfile(rfile, count=-1, sep=" ", dtype=np.float64)
+        
+    # Convert the rest of the data to the proper shape
+    if iformat == 1:
+        # We have a normal total intensity image
+        image.stokes = False
+        data = np.reshape(data, [image.nfreq, image.ny, image.nx])
+        if indexorder=='fortran':
+            data = np.swapaxes(data, 0, 2)
+
+    elif iformat == 3:
+        # We have the full stokes image
+        image.stokes = True
+        data = np.reshape(data, [image.nfreq, 4, image.ny, image.nx])
+        if indexorder=='fortran':
+            data = np.swapaxes(data, 0, 3)
+            data = np.swapaxes(data, 1, 2)
+
+    else:
+        msg = 'Unknown format number in image.out'
+        raise ValueError(msg)
+
+    # Add this to the object
+    image.image = data
+    
+    # Conversion from erg/s/cm/cm/Hz/ster to Jy/pixel
+    conv = image.sizepix_x * image.sizepix_y / pc**2. * 1e23
+    image.imageJyppix = image.image * conv
+    
+    # Create the x and y axes in units of cm
+    image.x = ((np.arange(image.nx, dtype=np.float64) + 0.5) - image.nx / 2) * image.sizepix_x
+    image.y = ((np.arange(image.ny, dtype=np.float64) + 0.5) - image.ny / 2) * image.sizepix_y
+    
+    # Return object
+    return image
+
