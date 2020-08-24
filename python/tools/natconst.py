ff935104eddfc4abce567535b4e3b1a3bf92bd5e
Cornelis Dullemond <dullemond@uni-heidelberg.de>
2020-08-24 12:44:10 +0200

    Adding natconst.py to the tools

---
+++ python/tools/natconst.py
@@ -0,0 +1,64 @@
+#
+# Name of the unit system
+#
+unitsystem = "CGS"
+#
+# Natural constants in CGS
+#
+#GG  = 6.673e-8       # Gravitational constant
+GG  = 6.67408e-08    # Gravitational constant  [cm^3/g/s^2]
+mp  = 1.6726e-24     # Mass of proton          [g]
+me  = 9.1095e-28     # Mass of electron        [g]
+kk  = 1.3807e-16     # Bolzmann's constant     [erg/K]
+hh  = 6.6262e-27     # Planck's constant       [erg.s]
+ee  = 4.8032e-10     # Unit charge             
+cc  = 2.9979245800000e10      # Light speed             [cm/s]
+st  = 6.6524e-25     # Thompson cross-section  [cm^2]
+ss  = 5.6703e-5      # Stefan-Boltzmann const  [erg/cm^2/K^4/s]
+aa  = 7.5657e-15     # 4 ss / cc               [erg/cm^3/K^4]
+#                                                              
+# Gas constants                                            
+#                                                              
+muh2= 2.3000e0       # Mean molec weight H2+He+Metals
+mole= 6.02214129e23  # 1 Mole (Avogadro's constant) = 1/mu
+#                                                              
+# Alternative units                                       
+#                                                              
+ev  = 1.6022e-12     # Electronvolt            [erg]
+kev = 1.6022e-9      # Kilo electronvolt       [erg]
+micr= 1.e-4          # Micron                  [cm]
+km  = 1.e5           # Kilometer               [cm]
+angs= 1.e-8          # Angstroem               [cm]
+#                                                              
+# Astronomy constants                                      
+#                                                              
+LS  = 3.8525e33      # Solar luminosity        [erg/s]
+RS  = 6.96e10        # Solar radius            [cm]
+MS  = 1.98892e33     # Solar mass              [g]
+TS  = 5.78e3         # Solar temperature       [K]
+AU  = 1.49598e13     # Astronomical Unit       [cm]
+au  = 1.49598e13     # Astronomical Unit       [cm]
+pc  = 3.08572e18     # Parsec                  [cm]
+Mea = 5.9736e27      # Mass of Earth           [g]
+Rea = 6.375e08       # Equatorial radius Earth [cm]
+Mmo = 7.347e25       # Mass of moon            [g]
+Rmo = 1.738e08       # Radius of moon          [cm]
+dmo = 3.844e10       # Distance earth-moon (center-to-center)  [cm]
+Mju = 1.899e30       # Mass of Jupiter         [g]
+Rju = 7.1492e9       # Equat. radius Jupiter   [rm]
+dju = 7.78412e13     # Distance Jupiter-Sun    [cm]
+Jy  = 1e-23          # Jansky                  [erg/s/cm^2/Hz]
+#                                                              
+# Time units                                               
+#                                                              
+year= 31557600.e0    # Year                    [s]
+hour= 3.6000e3       # Hour                    [s]
+day = 8.64e4         # Day                     [s]
+#
+# Math constants
+#
+pi  = 3.1415926535897932385e0 
+#
+# Miscellaneous
+#
+bar = 1e6            # One bar = 1e6 barye     [Ba]
