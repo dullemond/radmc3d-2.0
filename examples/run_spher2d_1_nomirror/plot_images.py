71c94f3145065c71f9ec16f6d690ba144e6d2d89
Cornelis Dullemond <dullemond@uni-heidelberg.de>
2020-08-29 16:25:19 +0200

    Updated run_spher2d_1_nomirror likewise.

--- examples/run_spher2d_1_nomirror/plot_images.py
+++ examples/run_spher2d_1_nomirror/plot_images.py
@@ -4,7 +4,6 @@ import numpy as np
 import math
 import os
 
-# Make sure that the shell variable PYTHONPATH points to the RADMC-3D python directory
 from radmc3dPy.image import *    
 from radmc3dPy.analyze import *  
 from radmc3dPy.natconst import * 
@@ -18,44 +17,7 @@ from radmc3dPy.natconst import *
 #
 #   radmc3d mctherm
 #
-os.system('radmc3d mctherm')
-#
-# to compute the dust temperature before you run this plotting session.
-#
-# Now plot the temperature profile
-#
-a    = readData(dtemp=True,binary=False)
-r    = a.grid.x[:]
-temp = a.dusttemp[:,-1,0,0]
-plt.figure(1)
-plt.plot(r/au,temp)
-plt.xlabel('r [au]')
-plt.ylabel('T [K]')
-plt.show()
-
-#
-# Now make sure to have run 
-#
-#   radmc3d sed incl 60
-#
-os.system('radmc3d sed incl 60')
-#
-# to get the spectral energy distribution
-#
-s    = readSpectrum()
-plt.figure(2)
-lammic = s[:,0]
-flux   = s[:,1]
-nu     = 1e4*cc/lammic
-nufnu  = nu*flux
-nulnu  = nufnu*4*math.pi*pc*pc
-plt.plot(lammic,nulnu/ls)
-plt.xscale('log')
-plt.yscale('log')
-plt.xlabel(r'$\lambda$ [$\mu$m]')
-plt.ylabel(r'$\nu L_{\nu}$ [$L_{\odot}$]')
-plt.axis([1e-1,1e4, 1e-6,1e1])
-plt.show()
+#os.system('radmc3d mctherm')
 
 #
 # Now make sure to have run 
@@ -129,48 +91,4 @@ plt.xlabel(r'$\phi\; [deg]$')
 plt.ylabel(r'$^{10}\log(r)\; [\mathrm{AU}]$')
 cbar=plt.colorbar()
 cbar.set_label(r'$^{10}\log(I_\nu)\;[\mathrm{erg}\,\mathrm{cm}^{-2}\,\mathrm{s}^{-1}\,\mathrm{Hz}^{-1}\,\mathrm{ster}^{-1}\,]$')
-plt.show()
-
-#
-# Now compare the SED created using the rectangular images
-# (standard method) and the SED created using the circular
-# images.
-#
-# Make sure to have run 
-#
-#   radmc3d sed circ
-#   cp spectrum.out spectrum_circ.out
-#   radmc3d sed
-#   cp spectrum.out spectrum_rect.out
-#
-os.system('radmc3d sed circ incl 60')
-os.system('cp spectrum.out spectrum_circ.out')
-os.system('radmc3d sed incl 60')
-os.system('cp spectrum.out spectrum_rect.out')
-#
-# and notice the difference in speed. The results should be 
-# mostly identical, but of course because the pixel arrangements
-# are a bit different the results will deviate a bit. Comparison
-# between these two spectra can also be used to check the accuracy
-# of the spectrum.
-#
-srect  = readSpectrum(fname='spectrum_rect.out')
-scirc  = readSpectrum(fname='spectrum_circ.out')
-plt.figure(6)
-lammic = srect[:,0]
-nu     = 1e4*cc/lammic
-fluxr  = srect[:,1]
-nufnur = nu*fluxr
-nulnur = nufnur*4*math.pi*pc*pc
-fluxc  = scirc[:,1]
-nufnuc = nu*fluxc
-nulnuc = nufnuc*4*math.pi*pc*pc
-plt.plot(lammic,nulnur/ls,label='SED (from rect images)')
-plt.plot(lammic,nulnuc/ls,'o',label='SED (from circ images)')
-plt.xscale('log')
-plt.yscale('log')
-plt.xlabel(r'$\lambda$ [$\mu$m]')
-plt.ylabel(r'$\nu L_{\nu}$ [$L_{\odot}$]')
-plt.axis([1e-1,1e4, 1e-6,1e1])
-plt.legend()
 plt.show()
