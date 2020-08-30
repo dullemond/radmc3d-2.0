0832279c66c752366323bb0124fa6971f8ae1bc0
Cornelis Dullemond <dullemond@uni-heidelberg.de>
2020-08-30 14:40:40 +0200

    First version of run_ppdisk_simple_3

--- examples/run_ppdisk_simple_3/plot_structure.py
+++ examples/run_ppdisk_simple_3/plot_structure.py
@@ -28,27 +28,3 @@ ax   = fig1.gca(projection='3d')
 ax.plot_surface(np.log10(rr)/au, zzr, np.log10(rhod), rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=1, antialiased=False)
 #ax.set_zlim3d(0, 10)
 
-#
-# Make and plot an example image
-#
-fig2  = plt.figure()
-makeImage(npix=200,incl=60.,phi=30.,wav=3.,sizeau=2)   # This calls radmc3d 
-a=readImage()
-plotImage(a,log=True,au=True,maxlog=6,cmap='hot')
-
-#
-# Make and plot the SED as seen at 1 pc distance
-#
-#os.system("radmc3d sed incl 60 phi 30")
-#fig3  = plt.figure()
-#s     = readSpectrum()
-#lam   = s[:,0]
-#nu    = 1e4*cc/lam
-#fnu   = s[:,1]
-#nufnu = nu*fnu
-#plt.plot(lam,nufnu)
-#plt.xscale('log')
-#plt.yscale('log')
-#plt.axis([1e-1, 1e4, 1e-10, 1e-4])
-#plt.xlabel('$\lambda\; [\mu \mathrm{m}$]')
-#plt.ylabel('$\\nu F_\\nu \; [\mathrm{erg}\,\mathrm{cm}^{-2}\,\mathrm{s}^{-1}]$')
