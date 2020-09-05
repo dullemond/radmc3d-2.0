5c1c7a9e3b7d60a1ce3a094c09f52d90c0dc5901
Cornelis Dullemond <dullemond@uni-heidelberg.de>
2020-09-05 20:48:55 +0200

    Working on run_ppdisk_analytic_3 (still to be completed).

---
+++ examples/run_ppdisk_analytic_3/plot_structure.py
@@ -0,0 +1,82 @@
+import problem_setup as p
+import numpy as np
+from mpl_toolkits.mplot3d import axes3d
+from matplotlib import pyplot as plt
+from matplotlib import cm
+from radmc3dPy.image import *
+from radmc3dPy.analyze import *
+from radmc3dPy.natconst import *
+
+#
+# Make sure to have done the following beforhand:
+#
+#  First compile RADMC-3D
+#  Then run:
+#   python problem_setup.py
+#   radmc3d mctherm
+#
+
+#
+# Read the data
+#
+d     = readData()
+rr,tt = np.meshgrid(d.grid.x,d.grid.y,indexing='ij')
+zzr   = np.pi/2-tt
+rhod  = d.rhodust[:,:,0,0]
+temp  = d.dusttemp[:,:,0,0]
+
+#
+# View a surface plot of the density structure
+#
+fig1 = plt.figure()
+ax   = fig1.gca(projection='3d')
+ax.plot_surface(np.log10(rr)/au, zzr, np.log10(rhod), rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=1, antialiased=False)
+
+#
+# Set the radii where to make the following plots
+#
+rpl = np.array([0.5,3,15,90])*au   # Radii where to make the plots
+irr = np.array(np.interp(rpl,d.grid.x,np.arange(len(d.grid.x)))+0.5,dtype=int)  # Nearest radial grid point
+
+#
+# Plot the vertical density structure at different radii
+#
+plt.figure()
+for ir in irr:
+    r    = d.grid.x[ir]
+    rstr = '{0:4.0f}'.format(r/au)
+    rstr = 'r = '+rstr.strip()+' au'
+    plt.semilogy(zzr[ir,:],rhod[ir,:],label=rstr)
+plt.ylim((1e-22,1e-12))
+plt.xlabel(r'$\pi/2-\theta\simeq z/r$')
+plt.ylabel(r'$\rho_{\mathrm{dust}}\;[\mathrm{g}/\mathrm{cm}^3]$')
+plt.legend()
+
+#
+# Plot the vertical temperature structure at different radii
+#
+plt.figure()
+for ir in irr:
+    r    = d.grid.x[ir]
+    rstr = '{0:4.0f}'.format(r/au)
+    rstr = 'r = '+rstr.strip()+' au'
+    plt.plot(zzr[ir,:],temp[ir,:],label=rstr)
+plt.xlabel(r'$\pi/2-\theta\simeq z/r$')
+plt.ylabel(r'$T_{\mathrm{dust}}\;[\mathrm{K}]$')
+plt.legend()
+
+#
+# Plot the radial midplane and surface temperature,
+# and compare to the analytic estimate
+#
+plt.figure()
+plt.semilogx(d.grid.x/au,d.dusttemp[:,0,0],':',label='RADMC-3D temperature (surface)',color='C0')
+plt.semilogx(d.grid.x/au,d.dusttemp[:,-1,0],label='RADMC-3D temperature (midplane)',color='C1')
+plt.semilogx(d.grid.x/au,d.dusttemp[:,-1,0],'.',color='C1')
+plt.semilogx(p.r/au,p.tmid,label='Analytic temperature (midplane)',color='C2')
+plt.xlabel(r'$r\;[\mathrm{au}]$')
+plt.ylabel(r'$T\;[\mathrm{K}]$')
+plt.ylim((0,2100))
+plt.legend()
+
+plt.show()
