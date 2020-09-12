12de8d2e0d6936e00be4c54257b83cbd0cba595a
Cornelis Dullemond <dullemond@uni-heidelberg.de>
2020-09-12 11:05:30 +0200

    Still working on the FARGO3D import. Getting there.

--- examples/run_import_fargo3d_1/problem_setup.py
+++ examples/run_import_fargo3d_1/problem_setup.py
@@ -64,6 +64,7 @@ r        = rc        # The radial grid of the analytic disk model (below)
 phii     = 0.5*(fargo.phi[1:]+fargo.phi[:-1])
 phii     = np.hstack([2*phii[0]-phii[1],phii,2*phii[-1]-phii[-2]])
 phic     = 0.5 * ( phii[:-1] + phii[1:] )
+nphi     = len(phic)
 r_2d,phi_2d = np.meshgrid(rc,phic,indexing='ij')
 
 #
@@ -125,6 +126,7 @@ a_grains    = a_grains_mic*1e-4          # Grain radii in cm
 xi_grains   = 3.7                        # Grain material density in g/cm^3
 m_grains    = (4*np.pi/3)*xi_grains*a_grains**3  # Grain masses
 s_grains    = np.pi*a_grains**2                  # Grain cross sections
+nrspec      = len(a_grains)
 
 #
 # We now compute the Stokes number of these grains at the
@@ -151,30 +153,19 @@ siggas       = 2e-15                     # Cross section of H2 molecule (assume 
 lmfp         = 1 / (n_gas_mid_2d*siggas) # Mean free path of the gas molecules
 assert (a_grains.max()/lmfp).max()<1, "Warning: Epstein regime not everywhere valid"
 
-
-
-
-
-
-
-"""
 #
-# The FARGO3D model has three dust species: Let's assume the first one consists
-# of 0.1 micron dust grains, which we assume will be vertically well-mixed
-# with the gas. Let's assume the
-# consisting of 100 micron dust grains which forms a settled dust midplane
-# layer that is 'settfact' settled.
+# Compute the turbulent viscosity in CGS units
 #
-fracbig  = 0.90              # Fraction of dust that is the big grain dust
-settfact = 0.1               # Settling factor of the big grains
-hp_biggr = hp*settfact
-hpr_biggr= hpr*settfact
+nu_fargo     = 1e-5                              # NOTE: This should be the one from the .par file of FARGO3D
+nu_cgs       = nu_fargo * np.sqrt(GG*mstar*r0)   # Turbulent viscosity in cm^2/s
+alpha_turb   = nu_cgs/(cs**2/omk)                # Just for fun: what would the turbulent alpha be?
+D_cgs        = nu_cgs                            # We assume the diffusion by turbulence = viscosity by turbulence
 
 #
 # Vertical grid parameters (theta-grid in spherical coordinates)
 #
 ntheta   = 32
-zrmax    = 0.5
+zrmax    = 0.5                     # You may need to fine tune this
 thetaup  = np.pi*0.5 - zrmax
 
 #
@@ -191,35 +182,42 @@ thetac   = 0.5 * ( thetai[:-1] + thetai[1:] )
 ntheta   = len(thetac)
 
 #
-# Make the phi coordinate
-#
-nphi     = 1
-phii     = np.linspace(0.e0,np.pi*2.e0,nphi+1)
-phic     = 0.5 * ( phii[0:nphi] + phii[1:nphi+1] )
-
-#
-# Make the 2-D grid (actually 3-D but with axisymmetry)
+# Make the 3-D grid
 #
 qq       = np.meshgrid(rc,thetac,phic,indexing='ij')
 rr       = qq[0]
 tt       = qq[1]
+pp       = qq[2]
 zr       = np.pi/2.e0 - qq[1]
 
 #
-# Expand the 1-D analytic model to 2-D
+# Expand the 2-D gas model to 3-D
 #
-sigmad_3d  = np.meshgrid(sigmad,thetac,phic,indexing='ij')[0]
-hh         = np.meshgrid(hp,thetac,phic,indexing='ij')[0]
-hhr        = np.meshgrid(hpr,thetac,phic,indexing='ij')[0]
-hh_biggr   = np.meshgrid(hp_biggr,thetac,phic,indexing='ij')[0]
-hhr_biggr  = np.meshgrid(hpr_biggr,thetac,phic,indexing='ij')[0]
+sigma_gas_3d  = np.zeros_like(rr)
+hh_3d         = np.zeros_like(rr)
+hhr_3d        = np.zeros_like(rr)
+for it in range(ntheta):
+    sigma_gas_3d[:,it,:] = fargo.sigma_gas_cgs[:,:]
+    hh_3d[:,it,:]        = hp_2d[:,:]
+    hhr_3d[:,it,:]       = hpr_2d[:,:]
+rho_gas_3d = ( sigma_gas_3d / (np.sqrt(2.e0*np.pi)*hh_3d) ) * np.exp(-(zr**2/hhr_3d**2)/2.e0)
+zhpr2_3d   = zr**2/hhr_3d**2
 
 #
-# Make the dust density model
+# Now do the same for the dust. Use Eq. (19) of Fromang & Nelson (2009) A&A 496, 597.
 #
-rhod_smlgr = (1.-fracbig) * ( sigmad_3d / (np.sqrt(2.e0*np.pi)*hh) ) * np.exp(-(zr**2/hhr**2)/2.e0)
-rhod_biggr = fracbig *      ( sigmad_3d / (np.sqrt(2.e0*np.pi)*hh_biggr) ) * np.exp(-(zr**2/hhr_biggr**2)/2.e0)
-
+rho_dust_3d = []
+for St,Sig in zip(Stokes_mid_2d,fargo.sigma_dust_cgs):
+    sigma_dust_3d  = np.zeros_like(rr)
+    St_3d          = np.zeros_like(rr)
+    D_3d           = np.zeros_like(rr)
+    for it in range(ntheta):
+        sigma_dust_3d[:,it,:] = Sig[:,:]
+        St_3d[:,it,:]         = St[:,:]
+        D_3d[:,it,:]          = D_cgs
+    rho = np.exp(-(St_3d/D_3d)*(np.exp(zhpr2_3d*0.5)-1)-zhpr2_3d*0.5)
+    rho_dust_3d.append(rho)
+    
 #
 # Monte Carlo parameters
 #
@@ -280,28 +278,23 @@ with open('amr_grid.inp','w+') as f:
 with open('dust_density.inp','w+') as f:
     f.write('1\n')                       # Format number
     f.write('%d\n'%(nr*ntheta*nphi))     # Nr of cells
-    f.write('2\n')                       # Nr of dust species
-    data = rhod_smlgr.ravel(order='F')   # Create a 1-D view, fortran-style indexing
-    data.tofile(f, sep='\n', format="%13.6e")
-    f.write('\n')
-    data = rhod_biggr.ravel(order='F')   # Create a 1-D view, fortran-style indexing
-    data.tofile(f, sep='\n', format="%13.6e")
-    f.write('\n')
+    f.write('{}\n'.format(nrspec))       # Nr of dust species
+    for rhod in rho_dust_3d:
+        data = rhod.ravel(order='F')   # Create a 1-D view, fortran-style indexing
+        data.tofile(f, sep='\n', format="%13.6e")
+        f.write('\n')
 #
 # Dust opacity control file
 #
 with open('dustopac.inp','w+') as f:
     f.write('2               Format number of this file\n')
-    f.write('2               Nr of dust species\n')
+    f.write('{}               Nr of dust species\n'.format(nrspec))
     f.write('============================================================================\n')
-    f.write('1               Way in which this dust species is read\n')
-    f.write('0               0=Thermal grain\n')
-    f.write('0.1_micron      Extension of name of dustkappa_***.inp file\n')
-    f.write('----------------------------------------------------------------------------\n')
-    f.write('1               Way in which this dust species is read\n')
-    f.write('0               0=Thermal grain\n')
-    f.write('100_micron      Extension of name of dustkappa_***.inp file\n')
-    f.write('----------------------------------------------------------------------------\n')
+    for agr in a_grains_mic:
+        f.write('1               Way in which this dust species is read\n')
+        f.write('0               0=Thermal grain\n')
+        f.write('{}_micron      Extension of name of dustkappa_***.inp file\n'.format(agr))
+        f.write('----------------------------------------------------------------------------\n')
 #
 # Write the radmc3d.inp control file
 #
@@ -309,6 +302,3 @@ with open('radmc3d.inp','w+') as f:
     f.write('nphot = %d\n'%(nphot))
     f.write('scattering_mode_max = 1\n')
     f.write('iranfreqmode = 1\n')
-
-
-"""
