c64ed997d4160c0213d8509192193acc5b3c999a
Cornelis Dullemond <dullemond@uni-heidelberg.de>
2020-09-17 21:11:20 +0200

    Now the results of optool and bhmie are identical: I added -p 0 -fmax 0 to the optool command line options, which did the trick.

--- opac/dust_continuum/compare_to_optool/create_opacity_with_optool.py
+++ opac/dust_continuum/compare_to_optool/create_opacity_with_optool.py
@@ -10,13 +10,13 @@ import os
 # And then put this directory into your shell PATH.
 # Or specify PATH here
 
-amic      = 0.1
+amic      = 10
 chopangle = 5.
 errortol  = 100
 
 path      = '/Users/cornelisdullemond/science/software/optool/'
-command   = path+'optool pyr-mg70 -a {} -s -radmc -chop {}'.format(amic,chopangle)
-#command   = path+'optool -c pyrmg70.lnk 1.0 3.01 -a {} -s -radmc -chop {}'.format(amic,chopangle)
+command   = path+'optool pyr-mg70 -a {} -s -p 0 -fmax 0 -radmc -chop {}'.format(amic,chopangle)
+#command   = path+'optool -c pyrmg70.lnk 1.0 3.01 -a {} -s -p 0 -fmax 0 -radmc -chop {}'.format(amic,chopangle)
 os.system(command)
 os.system('mv -f dustkapscatmat.inp dustkapscatmat_pyrmg70_optool.inp')
 
@@ -28,7 +28,7 @@ with open('wavelength_micron.inp','w') as f:
     f.write('{}\n'.format(len(optool.wav[0])))
     for w in optool.wav[0]:
         f.write('{0:13.6e}\n'.format(w))
-create_dustkapscatmat_file(amic,'pyrmg70',errortol=errortol,chopangle=chopangle,renametosize=False)
+create_dustkapscatmat_file(amic,'pyrmg70',errortol=errortol,chopangle=chopangle,renametosize=False,command="./makeopac")
 os.system('mv -f dustkapscatmat_pyrmg70.inp dustkapscatmat_pyrmg70_fortran.inp')
 
 #
@@ -43,6 +43,11 @@ plt.loglog(of.wav[0],of.kabs[0],label='F90')
 plt.loglog(op.wav[0],op.kabs[0],label='Optool')
 plt.loglog(of.wav[0],of.ksca[0],':',label='F90')
 plt.loglog(op.wav[0],op.ksca[0],':',label='Optool')
+plt.legend()
+
+plt.figure()
+plt.loglog(of.wav[0],of.phase_g[0],label='F90')
+plt.loglog(op.wav[0],op.phase_g[0],label='Optool')
 plt.legend()
 
 ilam = 100
