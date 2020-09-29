887c92a27871cd700b0652d5214df48241a367fe
Cornelis Dullemond <dullemond@uni-heidelberg.de>
2020-09-30 01:40:46 +0200

    Added README to run_ppdisk_fargo3d_1/ and set the phi angle of the camera viewpoint to 0.

--- examples/run_ppdisk_fargo3d_1_axisymm/plot_images.py
+++ examples/run_ppdisk_fargo3d_1_axisymm/plot_images.py
@@ -22,7 +22,7 @@ dpc = 140.     # Distance in parsec (for conversion to Jy/pixel in 1.3 mm map)
 #
 # Make and plot image of full disk at 1.3 mm: thermal dust emission
 #
-makeImage(npix=200,incl=60.,phi=30.,wav=1.3e3,setthreads=setthreads)   # This calls radmc3d 
+makeImage(npix=200,incl=60.,phi=0.,wav=1.3e3,setthreads=setthreads)   # This calls radmc3d 
 im_mm = readImage()
 plt.figure()
 plotImage(im_mm,au=True,log=True,maxlog=3,bunit='jy/pixel',dpc=dpc,cmap='magma')
@@ -30,7 +30,7 @@ plotImage(im_mm,au=True,log=True,maxlog=3,bunit='jy/pixel',dpc=dpc,cmap='magma')
 #
 # Make and plot image of full disk at 1 microns: scattered light
 #
-makeImage(npix=200,incl=60.,phi=30.,wav=1.,setthreads=setthreads)   # This calls radmc3d 
+makeImage(npix=200,incl=60.,phi=0.,wav=1.,setthreads=setthreads)   # This calls radmc3d 
 im_1 = readImage()
 plt.figure()
 plotImage(im_1,au=True,log=True,vmax=-10,vmin=-15,bunit='inu',cmap='hot')
