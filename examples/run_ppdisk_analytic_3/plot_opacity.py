4dc7f30fb81e10e63d429d79c2bc48155d7ee0a6
Cornelis Dullemond <dullemond@uni-heidelberg.de>
2020-11-24 22:52:41 +0100

    Updated run_ppdisk_analytic_3/plot_opacity.py

--- examples/run_ppdisk_analytic_3/plot_opacity.py
+++ examples/run_ppdisk_analytic_3/plot_opacity.py
@@ -6,12 +6,15 @@ from radmc3dPy.natconst import *
 #
 # Plot the opacity table
 #
-o    = readOpac(ext='silicate')
+o1    = readOpac(ext='0.1_micron')
+o2    = readOpac(ext='100_micron')
 plt.figure()
-plt.loglog(o.wav[0],o.kabs[0],label=r'$\kappa_\nu^{\mathrm{abs}}$ (absorption)')
-plt.loglog(o.wav[0],o.ksca[0],':',label=r'$\kappa_\nu^{\mathrm{scat}}$ (scattering)')
+plt.loglog(o1.wav[0],o1.kabs[0],label=r'$\kappa_\nu^{\mathrm{abs}}$ (absorption)',color='C0')
+plt.loglog(o1.wav[0],o1.ksca[0],':',label=r'$\kappa_\nu^{\mathrm{scat}}$ (scattering)',color='C0')
+plt.loglog(o2.wav[0],o2.kabs[0],label=r'$\kappa_\nu^{\mathrm{abs}}$ (absorption)',color='C1')
+plt.loglog(o2.wav[0],o2.ksca[0],':',label=r'$\kappa_\nu^{\mathrm{scat}}$ (scattering)',color='C1')
 plt.ylim((1e-2,1e5))
 plt.xlabel(r'$\lambda\;[\mu\mathrm{m}]$')
 plt.ylabel(r'$\kappa_\nu\;[\mathrm{cm}^2/\mathrm{g}]$')
-plt.title(r'Dust opacity (olivine, $a=0.1\,\mu\mathrm{m}$)')
+plt.title(r'Dust opacity (olivine, $a=0.1$ and $100\,\mu\mathrm{m}$)')
 plt.legend()
