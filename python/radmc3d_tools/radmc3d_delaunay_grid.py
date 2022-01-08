Fixed several issues in the python routines. One of the issues was with the calculation of the volumes: That did not work well with the large numbers. So I rescaled the numbers by the size of the object, and scaled back. That seemed to work well.

python/radmc3d_tools/radmc3d_delaunay_grid.py
@@ -5,7 +5,7 @@ import struct
 from tqdm import tqdm    # For a nice progress bar
 
 class Delaunaygrid(object):
-    def __init__(self,vertices,save_cellcenters=False,save_vertices=False):
+    def __init__(self,vertices,save_cellcenters=False,save_vertices=True):
         """
         Create a Delaunay grid for RADMC-3D from a set of 3D vertices. Note that
         this means that the vertices are not representative of the cells. The
@@ -37,11 +37,18 @@ class Delaunaygrid(object):
         self.save_vertices    = save_vertices
         self.save_size        = False   # For now
         #
+        # The Qhull algorithms are sensitive to very large numbers, so let's
+        # scale everything. First compute a useful scale factor
+        #
+        scale            = points.max(axis=0)-points.min(axis=0)
+        scale            = scale.max()
+        self.scale       = scale
+        #
         # Create the Delaunay Triangulation
         #
         print('Running scipy.spatial.Delaunay() to create Delaunay triangulation...')
-        self.tri         = Delaunay(vertices)
-        self.vertices    = self.tri.points
+        self.tri         = Delaunay(vertices/scale)
+        self.vertices    = self.tri.points*scale
         self.nverts      = vertices.shape[0]
         self.ncells      = self.tri.nsimplex
         self.ncells_open = 0
@@ -76,12 +83,13 @@ class Delaunaygrid(object):
         """
         Compute the volume of the Delaunay cells
         """
+        scale = self.scale
         self.cell_volumes = np.zeros(self.ncells)
         if useCH:
             #for i in range(self.tri.nsimplex):  # Without progress bar
             for i in tqdm(range(self.ncells)):
                 indices = self.tri.simplices[i]
-                self.cell_volumes[i] = ConvexHull(self.vertices[indices]).volume
+                self.cell_volumes[i] = scale**3 * ConvexHull(self.vertices[indices]/scale).volume
         else:
             v  = self.vertices[self.cell_iverts]
             cp = self.crossp((v[:,1,:]-v[:,-1,:]),(v[:,2,:]-v[:,-1,:]))

python/radmc3d_tools/radmc3d_voronoi_grid.py
@@ -30,11 +30,19 @@ class Voronoigrid(object):
         self.save_cellcenters = True    # Must be!
         self.save_size        = False   # For now
         #
+        # The Qhull algorithms are sensitive to very large numbers, so let's
+        # scale everything. First compute a useful scale factor
+        #
+        scale            = points.max(axis=0)-points.min(axis=0)
+        scale            = scale.max()
+        self.scale       = scale
+        #
         # Create the Voronoi tesselation
         #
         print('Running scipy.spatial.Voronoi() to create Voronoi tesselation...')
-        self.vor         = Voronoi(points)
-        self.cell_points = self.vor.points
+        self.vor         = Voronoi(points/scale)
+        self.cell_points = self.vor.points*scale
+        self.vertices    = self.vor.vertices*scale
         self.npnts       = points.shape[0]
         self.nverts      = 0       # For now
         #
@@ -72,6 +80,7 @@ class Voronoigrid(object):
         From https://stackoverflow.com/questions/19634993/volume-of-voronoi-cell-python
         Answer by ybeltukov
         """
+        scale = self.scale
         self.cell_volumes = np.zeros(self.vor.npoints)
         #for i in range(self.npnts):  # Without progress bar
         for i in tqdm(range(self.npnts)):
@@ -79,7 +88,7 @@ class Voronoigrid(object):
             if -1 in indices: # some regions can be open
                 self.cell_volumes[i] = 0.0
             else:
-                self.cell_volumes[i] = ConvexHull(self.vor.vertices[indices]).volume
+                self.cell_volumes[i] = scale**3 * ConvexHull(self.vertices[indices]/scale).volume
 
     def bbox_reject_cells(self):
         """
@@ -93,9 +102,9 @@ class Voronoigrid(object):
         #
         # Find the Voronoi vertices that are outside of the bbox
         #
-        maskx = np.logical_or(vor.vertices[:,0]<bbox[0][0],vor.vertices[:,0]>bbox[0][1])
-        masky = np.logical_or(vor.vertices[:,1]<bbox[1][0],vor.vertices[:,1]>bbox[1][1])
-        maskz = np.logical_or(vor.vertices[:,2]<bbox[2][0],vor.vertices[:,2]>bbox[2][1])
+        maskx = np.logical_or(self.vertices[:,0]<bbox[0][0],self.vertices[:,0]>bbox[0][1])
+        masky = np.logical_or(self.vertices[:,1]<bbox[1][0],self.vertices[:,1]>bbox[1][1])
+        maskz = np.logical_or(self.vertices[:,2]<bbox[2][0],self.vertices[:,2]>bbox[2][1])
         mask  = np.logical_or(maskx,np.logical_or(masky,maskz))
         ivout = set(np.where(mask)[0])
         #
@@ -108,6 +117,7 @@ class Voronoigrid(object):
                 self.cell_volumes[i] = 0.0
         
     def create_cellwalls(self):
+        points           = self.cell_points
         self.nwalls      = self.vor.ridge_points.shape[0]
         #
         # The support vectors
@@ -134,7 +144,8 @@ class Voronoigrid(object):
 
     def compute_diagnostics(self):
         self.ncells        = self.npnts
-        self.ncells_open   = len(np.where((self.cell_volumes<=0.0))[0])
+        self.cell_iopen    = np.where((self.cell_volumes<=0.0))[0]
+        self.ncells_open   = len(self.cell_iopen)
         self.ncells_closed = len(np.where((self.cell_volumes>0.0))[0])
         assert self.ncells_open+self.ncells_closed==self.ncells, 'Internal error.'
         self.volume_total   = self.cell_volumes.sum()
@@ -160,7 +171,7 @@ class Voronoigrid(object):
             for iwall in w:
                 ivert = vor.ridge_vertices[iwall].copy()
                 ivert.pop(-1)
-                polys.append(vor.vertices[ivert])
+                polys.append(self.vertices[ivert])
         pc = a3.art3d.Poly3DCollection(polys, facecolor=colors, edgecolor="k", alpha=alpha)
         ax.add_collection3d(pc)
         if bbox is None: