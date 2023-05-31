b7554ef5219bf1f93ea2e46aa485798db25889ed
Cornelis Dullemond <dullemond@uni-heidelberg.de>
2023-05-31 23:19:11 +0200

    Now included the artificial line widening into the example of run_lines_lte_3d_3/

--- examples/run_lines_lte_3d_3/problem_plotexample.py
+++ examples/run_lines_lte_3d_3/problem_plotexample.py
@@ -8,8 +8,14 @@ im_nocatch = readImage()
 os.system('radmc3d image iline 1 vkms 10 incl 60 phi 30 doppcatch')
 im_catch = readImage()
 
+os.system('radmc3d image iline 1 vkms 10 incl 60 phi 30 linewideningfactor 0.3')
+im_widen = readImage()
+
 plt.figure(1)
 plotImage(im_nocatch,log=True,cmap=cm.hot,bunit='snu',dpc=140,arcsec=True,vmin=-10.5,vmax=-4.0)
 
 plt.figure(2)
 plotImage(im_catch,log=True,cmap=cm.hot,bunit='snu',dpc=140,arcsec=True,vmin=-10.5,vmax=-4.0)
+
+plt.figure(3)
+plotImage(im_widen,log=True,cmap=cm.hot,bunit='snu',dpc=140,arcsec=True,vmin=-10.5,vmax=-4.0)
